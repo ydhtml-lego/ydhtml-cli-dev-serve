fa49d0a51e5065926b76ec16eb00a966b8ca5930
林间有风 <1538795093@qq.com>
2021-04-13 15:56:02 +0800

    first commit

---
+++ config/db.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** MONGODB  **/
+const mongodbUrl = 'mongodb://root:000000@book.youbaobao.xyz:27017/ydhtml-cli';
+const mongodbDbName = 'ydhtml-cli';
+
+module.exports = {
+  mongodbUrl,
+  mongodbDbName,
+};
